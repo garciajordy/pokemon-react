a31f507261edb2226fbb9b3ce2e7e2da90900d05
garciajordy <jordygarcia0119@gmail.com>
2021-06-03 09:25:21 +0300

    Add pagination

---
+++ src/components/Pagination.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Pagination({ goToPrevPage, goToNextPage }) {
+  return (
+    <div>
+      {goToPrevPage && <button type="button" onClick={goToPrevPage}>Previous</button>}
+      {goToNextPage && <button type="button" onClick={goToNextPage}>Next</button>}
+    </div>
+  );
+}
+Pagination.propTypes = {
+  goToPrevPage: PropTypes.func.isRequired,
+  goToNextPage: PropTypes.func.isRequired,
+
+};
+export default Pagination;
