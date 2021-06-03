aa4873e42d72aab02636aa377f1ffd264feb346d
garciajordy <jordygarcia0119@gmail.com>
2021-06-03 09:26:00 +0300

    Add all combineReducers

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import PokemonList from './PokemonList';
+
+function App() {
+  return (
+    <div className="App">
+      <PokemonList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import PokemonReducer from './pokemonReducer';
+
+const reducers = combineReducers({
+  PokemonReducer,
+});
+
+export default reducers;
