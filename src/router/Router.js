ee2b5b2cf51088fa5ec61d1a81be892cda5da765
garciajordy <jordygarcia0119@gmail.com>
2021-06-03 16:11:13 +0300

    Add router

--- src/router/Router.js
+++ src/router/Router.js
@@ -5,19 +5,15 @@ import {
   Switch,
 } from 'react-router-dom';
 import App from '../components/App';
+import Show from '../components/Show';
 
 const Routes = () => (
   <BrowserRouter>
-    <header>
-      {/* <nav>
-        <a className="nav-test" href="/">Home</a>
-        <a href="/calculator">Calculator</a>
-        <a href="/quote">Quote</a>
-      </nav> */}
-    </header>
+
     <Switch>
       <div>
         <Route exact path="/" component={App} />
+        <Route exact path="/show" component={Show} />
       </div>
     </Switch>
   </BrowserRouter>
